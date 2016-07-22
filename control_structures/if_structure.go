d3cb8f0c98b3998a7d5d0581411024c8956d8d5b
Sreekanth R <r.sreekanth@grofers.com>
2016-07-22 15:27:25 +0530

    if test added

--- control_structures/if_structure.go
+++ control_structures/if_structure.go
@@ -1,5 +1,13 @@
 package control_structures
-
+import "fmt"
 func fizzBuzz(i int32) string {
-	return ""
+	if i% 3 ==0 && i % 5 ==0 {
+		return "FizzBuzz"
+	} else if i % 5 ==0 {
+		return "Buzz"
+	} else if i%3==0 {
+		return "Fizz"
+	}else {
+		return fmt.Sprintf("%d",i)
+	}
 }
