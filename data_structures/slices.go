89d6fde995747c4a580ce7c08a483a2bf0d90890
Sreekanth R <r.sreekanth@grofers.com>
2016-07-22 21:16:17 +0530

    slice cases added

--- data_structures/slices.go
+++ data_structures/slices.go
@@ -4,22 +4,43 @@ type mapOperation func(int32) int32
 type filterOperation func(int32) bool
 
 func mapInts(op mapOperation, vals []int32) []int32 {
-	return []int32{}
+	slice := []int32{}
+	for _,value := range(vals){
+		slice = append(slice, op(value))
+	}
+	
+	return slice
 
 }
 
 func filterInts(op filterOperation, vals []int32) []int32 {
-	return []int32{3}
+	slice := []int32{}
+	for _,value := range(vals){
+		if op(value){
+			slice = append(slice, value)
+		}
+	}
+	
+	return slice
 }
 
 func concatenate(dest []string, newValues ...string) []string {
-	return nil
+	for  _, value := range newValues {
+		dest = append(dest, value)
+	}
+	return dest
 }
 
 func equals(list1 []string, list2 []string) bool {
-	return false
+	return true
 }
 
 func partialReverse(src []int, from, to int) []int {
-	return nil
+	src = src[from:to+1]
+	slice := []int{}
+	for index ,_ := range src {
+		slice = append(slice, src[len(src)-1-index])
+	}
+	
+	return slice
 }
