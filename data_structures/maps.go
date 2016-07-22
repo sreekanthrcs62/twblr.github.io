84a1b45d06597c3e523e4c779994c938bdfece61
Sreekanth R <r.sreekanth@grofers.com>
2016-07-22 18:17:17 +0530

    Map case added

--- data_structures/maps.go
+++ data_structures/maps.go
@@ -1,9 +1,15 @@
 package data_structures
 
 func findPeopleWithCommonInterest(data map[string][]string, interest string) []string {
-	return nil
+	people := []string{}
+	for key,valueA := range data {
+		if contains(valueA, interest) {
+			people = append(people, key)
+		}
+	}
+	return people
 }
 
 func contains(src []string, elem string) bool {
-	return false
+	return true
 }
