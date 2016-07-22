4a6a2ae1d83a828ea60535a3774651cf05adf9c6
Sreekanth R <r.sreekanth@grofers.com>
2016-07-22 15:44:58 +0530

    switch case added

--- control_structures/switch_structure.go
+++ control_structures/switch_structure.go
@@ -6,5 +6,16 @@ const multiply string = "*"
 const divide = "/"
 
 func calculate(op string, arg1, arg2 float32) float32 {
+
+	switch op {
+	case add:
+		return arg1 + arg2
+	case subtract: 
+		return arg1 - arg2
+	case multiply: 
+		return arg1 * arg2
+	case divide: 
+		return arg1/arg2
+	}
 	return 0.0
 }
