456a0c873cfa0bf9feeee95367f0948d229ec39b
Sreekanth R <r.sreekanth@grofers.com>
2016-07-22 16:08:26 +0530

    for case added

--- control_structures/for_structure.go
+++ control_structures/for_structure.go
@@ -1,5 +1,14 @@
 package control_structures
 
-func collatzChainLength(n int) int {
-	return 0
+func collatzChainLength(i int) int {
+	var count int
+	for i !=1 {
+		if i % 2 == 0 {
+			i = i/2
+		} else {
+			i = 3 * i + 1
+		}
+		count++
+	}
+	return count
 }
